89ad8461f804b0135fbde4f7f87099cac96c0e52
daniel-pan-moos <daniel-pan-moos@protonmail.com>
2020-08-24 15:33:02 +0200

    Buy token page updated

--- src/pages/Widgets.vue
+++ src/pages/Widgets.vue
@@ -67,9 +67,9 @@
         </div>
         
         <div class="row">
-          <div class="col-md-6">
-            <h4 class="card-title">Progress Bars</h4>
-            <base-progress label="Default" value-position="right" :value="25" />
+          <div class="col-md-6 offset-md-2">
+            <h4 class="card-title">Progress Bar</h4>
+            <base-progress label="Default" value-position="right" :value="67" />
             <!-- <base-progress
               label="Primary"
               :value="60"
@@ -84,8 +84,149 @@
       </div>
     </card>
     <!-- end card -->
+
+      <ValidationObserver v-slot="{ handleSubmit }">
+  <form class="form-horizontal" @submit.prevent="handleSubmit(submit)">
+    <card>
+      <h4 slot="header" class="card-title">Type Validation</h4>
+      <div>
+        <div class="row">
+          <label class="col-sm-2 col-form-label">Requred Text</label>
+          <div class="col-sm-7">
+            <ValidationProvider
+              name="required"
+              rules="required"
+              v-slot="{ passed, failed, errors }"
+            >
+            <base-input
+              required
+              v-model="required"
+              :error="errors[0]"
+              :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
+            </base-input>
+           </ValidationProvider>
+          </div>
+          <label class="col-sm-3 label-on-right"
+            ><code>required="true"</code></label
+          >
+        </div>
+
+        <div class="row">
+          <label class="col-sm-2 col-form-label">Email</label>
+          <div class="col-sm-7">
+            <ValidationProvider
+              name="email"
+              rules="required|email"
+              v-slot="{ passed, failed, errors }"
+            >
+            <base-input
+              required
+              v-model="email"
+              type="email"
+              :error="errors[0]"
+              :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
+            </base-input>
+           </ValidationProvider>
+          </div>
+          <label class="col-sm-3 label-on-right"
+            ><code>email="true"</code></label
+          >
+        </div>
+
+        <div class="row">
+          <label class="col-sm-2 col-form-label">Number</label>
+          <div class="col-sm-7">
+            <ValidationProvider
+              name="number"
+              rules="required|numeric"
+              v-slot="{ passed, failed, errors }"
+            >
+            <base-input
+              required
+              v-model="number"
+              :error="errors[0]"
+              :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
+            </base-input>
+           </ValidationProvider>
+          </div>
+          <label class="col-sm-3 label-on-right"
+            ><code>numeric="true"</code></label
+          >
+        </div>
+
+        <div class="row">
+          <label class="col-sm-2 col-form-label">Url</label>
+          <div class="col-sm-7">
+            <ValidationProvider
+              name="url"
+              :rules="{
+                required: true,
+                regex: /(https?:\/\/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9])(:?\d*)\/?([a-z_\/0-9\-#.]*)\??([a-z_\/0-9\-#=&]*)/g
+              }"
+              v-slot="{ passed, failed, errors }"
+            >
+            <base-input
+              v-model="url"
+              type="text"
+              :error="errors[0]"
+              :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
+            </base-input>
+           </ValidationProvider>
+          </div>
+          <label class="col-sm-3 label-on-right"><code>url="true"</code></label>
+        </div>
+
+        <div class="row">
+          <label class="col-sm-2 col-form-label">Url</label>
+          <div class="col-sm-3">
+            <ValidationProvider
+            name="equal"
+            rules="required|confirmed:confirmation"
+            v-slot="{ passed, failed, errors }"
+            >
+            <base-input
+              v-model="equal"
+              type="text"
+              :error="errors[0]"
+              :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
+            </base-input>
+           </ValidationProvider>
+          </div>
+          <div class="col-sm-3">
+            <ValidationProvider
+            name="equalTo"
+            rules="required"
+            vid="confirmation"
+            v-slot="{ passed, failed, errors }"
+            >
+            <base-input
+              v-model="equalTo"
+              type="text"
+              :error="errors[0]"
+              :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
+            </base-input>
+           </ValidationProvider>
+          </div>
+
+          <label class="col-sm-4 label-on-right"
+            ><code>confirmed="equalToSource"</code></label
+          >
+        </div>
+      </div>
+      <div class="text-center">
+        <base-button
+          native-type="submit"
+          type="primary"
+          >Validate inputs</base-button
+        >
+      </div>
+    </card>
+  </form>
+</ValidationObserver>
   </div>
+  
 </template>
+
 <script>
 import { TimeSelect, DatePicker, Select, Option } from 'element-ui';
 import {
@@ -93,6 +234,14 @@ import {
   BaseSwitch,
   Slider
 } from 'src/components/index';
+
+import { extend } from "vee-validate";
+import { required, numeric, regex, confirmed } from "vee-validate/dist/rules";
+
+extend("required", required);
+extend("numeric", numeric);
+extend("regex", regex);
+extend("confirmed", confirmed);
 
 export default {
   components: {
@@ -106,16 +255,13 @@ export default {
   },
   data() {
     return {
-      enabledRadio: '2',
-      disabledRadio: '2',
-      switches: {
-        defaultOn: true,
-        defaultOff: false,
-        plainOn: true,
-        plainOff: false,
-        withIconsOn: true,
-        withIconsOff: false
-      },
+      required: "",
+      email: "",
+      number: "",
+      url: "",
+      equal: "",
+      equalTo: "",
+      
       selects: {
         simple: '',
         countries: [
@@ -129,6 +275,11 @@ export default {
       dateTimePicker: '',
       timePicker: ''
     };
+  },
+  methods: {
+    submit() {
+      alert("Form has been submitted!");
+    }
   }
 };
 </script>
